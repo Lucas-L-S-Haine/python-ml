c8fa9edae2228f56097f4302f9730fee19050c8e
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-21 15:20:12 -0300

    feat: convert data frame to Excel format

--- src/pandas/titanic.py
+++ src/pandas/titanic.py
@@ -5,3 +5,6 @@ titanic = pd.read_csv("data/titanic.csv")
 
 # Read the first 8 entries
 print(titanic.head(8))
+
+# Convert data frame to Excel format
+titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
