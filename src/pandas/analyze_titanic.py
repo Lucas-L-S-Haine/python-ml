841940209f7d0c1a343f36ee103831b52d832f69
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-21 15:36:37 -0300

    feat: separate script into load and analize

--- src/pandas/analyze_titanic.py
+++ src/pandas/analyze_titanic.py
@@ -1,10 +1,7 @@
-import pandas as pd
-
-# Store titanic data as a data frame
-titanic = pd.read_csv("data/titanic.csv")
+from load_titanic import titanic
 
 # Read the first 8 entries
-# print(titanic.head(8))
+print(titanic.head(8))
 
 # Convert data frame to Excel format
 titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
---
+++ src/pandas/load_titanic.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+# Store titanic data as a data frame
+titanic = pd.read_csv("data/titanic.csv")
