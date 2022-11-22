104ed252e26a134e151fb0ee12228a72a64692d3
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-22 18:01:40 -0300

    feat: read data types

--- src/pandas/analyze_titanic.py
+++ src/pandas/analyze_titanic.py
@@ -7,3 +7,6 @@ print(titanic.head(8))
 titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
 # Read from Excel spreadsheet
 # table = pd.read_excel("titanic.xlsx", sheet_name="passengers")
+
+# Read data types
+print(titanic.dtypes)
