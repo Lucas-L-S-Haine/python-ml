332ca0bc9de17a5d3c1a424b181fbecf0ebe1efe
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-01 19:40:26 -0300

    feat: add more parameters to read_csv

--- src/pandas/load_data.py
+++ src/pandas/load_data.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 # Store titanic data as a data frame
 titanic = pd.read_csv("data/titanic.csv")
-air_quality = pd.read_csv("data/air_quality_no2.csv")
+air_quality = pd.read_csv("data/air_quality_no2.csv",
+                          index_col=0,
+                          parse_dates=True)
