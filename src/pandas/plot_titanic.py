f84f76844f11c23c71110ffcf75030a7090c9b49
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-02 19:20:19 -0300

    feat: plot a single series out of the data frame

--- src/pandas/plot_titanic.py
+++ src/pandas/plot_titanic.py
@@ -8,3 +8,7 @@ print(air_quality.head())
 # Plot data as a time series
 air_quality.plot()
 plt.show()
+
+# Plot a single series out of the data frame
+air_quality["station_paris"].plot()
+plt.show()
