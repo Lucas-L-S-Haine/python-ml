6dcf5116cf21397b3a416c190071fe4c5c2f6ea8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-03 12:09:19 -0300

    feat: create data series

---
+++ src/pandas/create_series.py
@@ -0,0 +1,11 @@
+from load_data import air_quality
+
+
+# Data frame with 3 series
+print(air_quality.head())
+
+# Create another series from station_london
+air_quality["london_mg_per_cubic"] = air_quality["station_london"] * 1.882
+
+# Print resulting data frame. This one now has 4 series
+print(air_quality.head())
