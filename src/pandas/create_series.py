affa191227b243d4bca68c784644a882b51ea108
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-09 21:10:16 -0300

    feat: create another series from two other ones

--- src/pandas/create_series.py
+++ src/pandas/create_series.py
@@ -10,3 +10,8 @@ air_quality["london_mg_per_cubic"] = air_quality["station_london"] * 1.882
 
 # Print resulting data frame. This one now has 4 series
 print(air_quality.head())
+
+air_quality["ratio_paris_antwerp"] = (
+    air_quality["station_paris"] / air_quality["station_antwerp"]
+)
+print(air_quality.head())
