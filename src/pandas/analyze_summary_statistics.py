0d735af43a80e4dbddb81e2c9bee8a5ae5a5d1b2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-18 19:02:34 -0300

    feat: use median method for a data frame

--- src/pandas/analyze_summary_statistics.py
+++ src/pandas/analyze_summary_statistics.py
@@ -4,3 +4,6 @@ from load_data import titanic
 
 # Calculate the arithmetic mean of age
 print(titanic["Age"].mean())
+
+# Calculate the median of two series
+print(titanic[["Age", "Fare"]].median())
