60adc7dd9945956ff453d5cf1709f30b5e5d9246
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-19 17:50:22 -0300

    refactor: add an extra line to each print

--- src/pandas/analyze_summary_statistics.py
+++ src/pandas/analyze_summary_statistics.py
@@ -3,7 +3,7 @@ from load_data import titanic
 
 
 # Calculate the arithmetic mean of age
-print(titanic["Age"].mean())
+print(titanic["Age"].mean(), end="\n\n")
 
 # Calculate the median of two series
-print(titanic[["Age", "Fare"]].median())
+print(titanic[["Age", "Fare"]].median(), end="\n\n")
