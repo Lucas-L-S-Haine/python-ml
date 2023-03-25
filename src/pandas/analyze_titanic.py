67de9e697d7f7bfd0bc57929c41bb325350c100a
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-03-25 19:13:18 -0300

    refactor: replace shebangs

--- src/pandas/analyze_titanic.py
+++ src/pandas/analyze_titanic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from load_data import titanic
 
 
--- src/pandas/plot_air_quality.py
+++ src/pandas/plot_air_quality.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 
 from load_data import air_quality
