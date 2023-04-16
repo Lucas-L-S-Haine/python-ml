0b45e256bbd7bbe5b2963f7ad21b933ce81493dd
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-16 10:23:20 -0300

    feat: sort by age and class

--- src/pandas/reshape_tables.py
+++ src/pandas/reshape_tables.py
@@ -5,3 +5,7 @@ from load_data import titanic, air_quality
 # Sort data by age
 sorted_data = titanic.sort_values(by="Age")
 print(sorted_data[["Name", "Age"]].head(8))
+
+# Sort by cabin class and age in descending order
+sorted_data = titanic.sort_values(by=['Pclass', 'Age'], ascending=False)
+print(sorted_data[["Name", "Age", "Pclass"]].head(8))
